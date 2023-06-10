6a83c139ce8daae1cfea1d9e56509af7b49662b6
Charlie Mickey <118931297+charlesmickey222@users.noreply.github.com>
2023-06-09 22:06:32 -0500

    locate text in txt file from root directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
+use std::fs;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("searching for {}", query);
+    println!("Searching for {}", query);
     println!("In file {}", file_path);
+    
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
+
+    println!("With text:\n{contents}");
 }
