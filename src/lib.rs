eee8f64096e840f56ccb26a68a0867ce2446a0ad
Charlie Mickey <118931297+charlesmickey222@users.noreply.github.com>
2023-06-16 14:39:20 -0500

    update config build and main to use iteratore instead of clone

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         // --snip--
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+          Some(arg) => arg,
+          None => return Err("No Query String"),
+        };
+        let file_path = match args.next(){
+          Some(arg) => arg,
+          None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config { 
+          query, 
+          file_path, 
+          ignore_case,
+        })
     }
 }
 
